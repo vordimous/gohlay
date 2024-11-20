89d6ca87cbb150f81c4778035de085e76c0fdda9
AJ <vordimous@outlook.com>
2024-11-20 14:13:58 -0500

    feat: allow headers to be customized

--- cmd/root.go
+++ cmd/root.go
@@ -31,7 +31,7 @@ func Execute() {
 }
 
 // dev options
-var cfgFileDir string
+var ConfigFileDir string
 var Silent bool
 var Verbose bool
 var Debug bool
@@ -41,13 +41,14 @@ var JsonOut bool
 var Deadline int64
 
 // kafka options
-var bootstrap_servers []string
+var BootstrapServers []string
 var Topics []string
+var HeaderOverrides string
 
 func init() {
 
 	// Dev
-	rootCmd.PersistentFlags().StringVar(&cfgFileDir, "config_dir", ".", "config file directory.")
+	rootCmd.PersistentFlags().StringVar(&ConfigFileDir, "config_dir", ".", "config file directory.")
 	if err := viper.BindPFlag("config_dir", rootCmd.PersistentFlags().Lookup("config_dir")); err != nil { log.Error(err) }
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Display more verbose output in console output.")
 	if err := viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose")); err != nil { log.Error(err) }
@@ -63,10 +64,12 @@ func init() {
 	if err := viper.BindPFlag("deadline", rootCmd.PersistentFlags().Lookup("deadline")); err != nil { log.Error(err) }
 
 	// Kafka
-	rootCmd.PersistentFlags().StringArrayVarP(&bootstrap_servers, "bootstrap_servers", "b", []string{"localhost:9092"}, "Sets the \"bootstrap.servers\" parameter in the kafka.ConfigMap")
+	rootCmd.PersistentFlags().StringArrayVarP(&BootstrapServers, "bootstrap_servers", "b", []string{"localhost:9092"}, "Sets the \"bootstrap.servers\" parameter in the kafka.ConfigMap")
 	if err := viper.BindPFlag("bootstrap_servers", rootCmd.PersistentFlags().Lookup("bootstrap_servers")); err != nil { log.Error(err) }
 	rootCmd.PersistentFlags().StringArrayVarP(&Topics, "topics", "t", []string{"gohlay"}, "Sets the kafka topics to use")
 	if err := viper.BindPFlag("topics", rootCmd.PersistentFlags().Lookup("topics")); err != nil { log.Error(err) }
+	rootCmd.PersistentFlags().StringVarP(&HeaderOverrides, "override_headers", "o", "", "Maps the name of default headers to a custom header `a=b,c=d`. ex: `-h GOHLAY=DELAY_TIME`")
+	if err := viper.BindPFlag("override_headers", rootCmd.PersistentFlags().Lookup("override_headers")); err != nil { log.Error(err) }
 }
 
 
