38ecc680eba72ee778bb527c496fa9e5ed7102dc
AJ <vordimous@outlook.com>
2024-11-30 15:11:15 -0500

    fix: update kafka config tests

--- config/kafka_test.go
+++ config/kafka_test.go
@@ -8,43 +8,16 @@ import (
 	"github.com/spf13/viper"
 )
 
-func Test_bootrapServersString(t *testing.T) {
-	tests := []struct {
-		name              string
-		want              string
-		bootstrap_servers []string
-	}{
-		{
-			name:              "Test single server",
-			bootstrap_servers: []string{"localhost:9092"},
-			want:              "localhost:9092",
-		},
-		{
-			name:              "Test multiple servers",
-			bootstrap_servers: []string{"localhost:9092", "localhost:9093"},
-			want:              "localhost:9092,localhost:9093",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			viper.Set("bootstrap_servers", tt.bootstrap_servers)
-			if got := bootrapServersString(); got != tt.want {
-				t.Errorf("bootrapServersString() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestConsumer(t *testing.T) {
 	tests := []struct {
 		name              string
 		bootstrap_servers []string
-		wantConfig        *kafka.ConfigMap
+		wantConfig        kafka.ConfigMap
 	}{
 		{
 			name:              "Test single server and topic",
 			bootstrap_servers: []string{"localhost:9092"},
-			wantConfig: &kafka.ConfigMap{
+			wantConfig: kafka.ConfigMap{
 				"bootstrap.servers":               "localhost:9092",
 				"go.application.rebalance.enable": true,
 				"session.timeout.ms":              6000,
@@ -55,7 +28,7 @@ func TestConsumer(t *testing.T) {
 		{
 			name:              "Test multiple servers and topics",
 			bootstrap_servers: []string{"localhost:9092", "localhost:9093"},
-			wantConfig: &kafka.ConfigMap{
+			wantConfig: kafka.ConfigMap{
 				"bootstrap.servers":               "localhost:9092,localhost:9093",
 				"go.application.rebalance.enable": true,
 				"session.timeout.ms":              6000,
@@ -67,6 +40,7 @@ func TestConsumer(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			viper.Set("bootstrap_servers", tt.bootstrap_servers)
+			readKafkaConfig()
 			gotConfig := Consumer()
 			if !reflect.DeepEqual(gotConfig, tt.wantConfig) {
 				t.Errorf("Consumer() gotConfig = %v, want %v", gotConfig, tt.wantConfig)
@@ -79,19 +53,19 @@ func TestProducer(t *testing.T) {
 	tests := []struct {
 		name              string
 		bootstrap_servers []string
-		wantConfig        *kafka.ConfigMap
+		wantConfig        kafka.ConfigMap
 	}{
 		{
 			name:              "Test single server and topic",
 			bootstrap_servers: []string{"localhost:9092"},
-			wantConfig: &kafka.ConfigMap{
+			wantConfig: kafka.ConfigMap{
 				"bootstrap.servers": "localhost:9092",
 			},
 		},
 		{
 			name:              "Test multiple servers and topics",
 			bootstrap_servers: []string{"localhost:9092", "localhost:9093"},
-			wantConfig: &kafka.ConfigMap{
+			wantConfig: kafka.ConfigMap{
 				"bootstrap.servers": "localhost:9092,localhost:9093",
 			},
 		},
@@ -99,8 +73,9 @@ func TestProducer(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			viper.Set("bootstrap_servers", tt.bootstrap_servers)
+			readKafkaConfig()
 			if gotConfig := Producer(); !reflect.DeepEqual(gotConfig, tt.wantConfig) {
-				t.Errorf("Producer() = %v, want %v", gotConfig, tt.wantConfig)
+				t.Errorf("Producer() gotConfig = %v, want %v", gotConfig, tt.wantConfig)
 			}
 		})
 	}
